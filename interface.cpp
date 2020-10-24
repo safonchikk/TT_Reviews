36951055c3523227fce4a0cc1448fbb377e0fec1
safonchikk <57540363+safonchikk@users.noreply.github.com>
2020-10-25 00:19:00 +0300

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -8,39 +8,39 @@ using std::pair;
 
 const int64_t MODULUS = 1000000007;
 
-struct Input_data{
-  int64_t nodes_number, moves_number;
-  vector <Vertex> edges;
+struct Edge{
+    int start_node, finish_node;
 };
 
-struct Edge{
-  int start_node, finish_node;
+struct Input_data{
+    int64_t nodes_number, moves_number;
+    vector<Edge> edges;
 };
 
 class Matrix {
  private:
-  int matrix_size;
-  vector <vector<int64_t>> matrix;
+    int matrix_size;
+    vector<vector<int64_t>> matrix;
 
-  //умножение матриц
-  void multiply(const Matrix &other_matrix);
+    //умножение матриц
+    void multiply(const Matrix &other_matrix);
 
-  //из списка смежности в матрицу смежности
-  void edges_to_matrix(const vector <Edge> &edges);
+    //из списка смежности в матрицу смежности
+    void edges_to_matrix(const vector <Edge> &edges);
 
  public:
-  explicit Matrix(const int matrix_size);
+    explicit Matrix(const int matrix_size);
 
-  explicit Matrix(const Input_data &labyrinth_parameters);
+    explicit Matrix(const Input_data &labyrinth_parameters);
 
-  //конструктор копирования
-  Matrix(const Matrix& other_matrix);
+    //конструктор копирования
+    Matrix(const Matrix& other_matrix);
 
-  //сумма первого ряда
-  const int64_t first_row_sum();
+    //сумма первого ряда
+    const int64_t first_row_sum();
 
-  //двоичное возведение в степень
-  void calculate_matrix_power(int power);
+    //двоичное возведение в степень
+    void calculate_matrix_power(int power);
 };
 
 //функция считающая пути (решающая задачу)
@@ -53,11 +53,11 @@ Input_data read(std::istream& in);
 void write(std::ostream& out, const int64_t number_of_ways);
 
 int main(){
-  std::ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  const auto &labyrinth_parameters = read(cin);
-  const int64_t number_of_ways = count_ways(labyrinth_parameters);
-  write(cout, number_of_ways);
-  return 0;
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    const auto &labyrinth_parameters = read(cin);
+    const int64_t number_of_ways = count_ways(labyrinth_parameters);
+    write(cout, number_of_ways);
+    return 0;
 }
