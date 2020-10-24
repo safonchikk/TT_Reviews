cef1bd9ec1e55c7eed1367c32674f1f66ad3ec68
safonchikk <57540363+safonchikk@users.noreply.github.com>
2020-10-24 22:31:57 +0300

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
 #include <vector>
 
-const int64_t MOD = 1000000007;
+using std::cin;
+using std::cout;
+using std::vector;
+using std::pair;
+
+const int64_t MODULUS = 1000000007;
+
+struct Input_data{
+  int64_t node_number, moves_number;
+  vector <pair <int, int> > vertices;
+};
 
 class Matrix {
- public:
+ private:
   int nodes;
-  std::vector <std::vector<int64_t>> matrix;
+  vector <vector<int64_t>> matrix;
 
-  explicit Matrix(int node_number);
-  Matrix(const Matrix& other_matrix);
-
-  void vertices_input(int vertex_number);
-
-  const int64_t first_row_sum();
-
+  //умножение матриц
   void multiply(const Matrix& other_matrix);
 
+  //из списка смежности в матрицу смежности
+  void vertices_to_matrix(const vector <pair <int, int> > &vertices);
+
+ public:
+  explicit Matrix(int node_number);
+
+  explicit Matrix(const Input_data &data);
+
+  //конструктор копирования
+  Matrix(const Matrix& other_matrix);
+
+  //сумма первого ряда
+  const int64_t first_row_sum();
+
+  //двоичное возведение в степень
   void binary_power(int pow);
 };
 
-Matrix read_matrix(int64_t* moves);
+//функция считающая пути (решающая задачу)
+int64_t count_ways(const Input_data &data);
 
-int64_t count_ways(Matrix Labyrinth, int64_t moves);
+//ввод
+Input_data read(std::istream& in);
+
+//вывод ответа
+void write(std::ostream& out, int64_t number_of_ways);
 
 int main(){
   std::ios_base::sync_with_stdio(false);
-  std::cin.tie(0);
-
-  int64_t moves;
-  Matrix Labyrinth = read_matrix(&moves);
-
-  std::cout << count_ways(Labyrinth, moves);
+  cin.tie(0);
+  cout.tie(0);
+  const auto &data = read(cin);
+  int64_t number_of_ways = count_ways(data);
+  write(cout, number_of_ways);
   return 0;
 }
